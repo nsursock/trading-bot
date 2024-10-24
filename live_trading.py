5bb82558ff57db671e3d7587351c2b3d123f977c
Nicolas Sursock <nsursock@Nicolass-MacBook-Air.local>
2024-10-24 10:03:21 +0200

    Changed to 1m by curiosity

--- live_trading.py
+++ live_trading.py
@@ -175,7 +175,7 @@ def on_open(ws):
     from parameters import create_financial_params, selected_params
     
     financial_params = create_financial_params(selected_params)
-    # financial_params['interval'] = '1m'  # for debugging
+    financial_params['interval'] = '1m'  # for debugging
     # financial_params['cooldown_period'] = 2
     financial_params['risk_per_trade'] = 0.018
     financial_params['initial_balance'] = 5000
